5105d502780e036aadd67677c452c6b3a00c92a6
Samuel Cornet <samuel@noukod.com>
2018-11-08 18:25:17 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -8,3 +8,18 @@ CREATE TABLE subgenres (
   id INTEGER PRIMARY KEY,
   name TEXT
 );
+CREATE TABLE Authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+-- CREATE TABLE Books (
+--   id INTEGER PRIMARY KEY,
+--   title TEXT,
+--   year DATE,
+--   series_id INTEGER
+-- );
+-- CREATE TABLE characters (
+--   id INTEGER PRIMARY KEY,
+--   name TEXT,
+--   sub-genre_id INTEGER
+-- );
